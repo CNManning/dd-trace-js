4f1dc29ae0c131706c19b4d6c256dcf5ebe38f57
rishabh <7551150+rishabh@users.noreply.github.com>
2019-07-09 10:23:04 -0400

    Initial commit
    cbas queries are broken, I need to figure out how to enable cbas programmatically (in the tests or in the Dockerfile)

---
+++ packages/datadog-plugin-couchbase/src/index.js
@@ -0,0 +1,154 @@
+'use strict'
+
+const Tags = require('../../../ext/tags')
+const Kinds = require('../../../ext/kinds')
+const analyticsSampler = require('../../dd-trace/src/analytics_sampler')
+const tx = require('../../dd-trace/src/plugins/util/tx')
+
+function startQuerySpan (queryType, resource, tracer, config) {
+  const childOf = tracer.scope().active()
+  const span = tracer.startSpan(`couchbase.call`, {
+    childOf,
+    tags: {
+      'db.type': 'couchbase',
+      'span.type': 'sql',
+      'component': 'couchbase',
+      'service.name': config.service || `${tracer._service}-couchbase`,
+      'resource.name': resource,
+      'query.type': queryType,
+      [Tags.SPAN_KIND]: Kinds.CLIENT
+    }
+  })
+
+  analyticsSampler.sample(span, config.analytics)
+  return span
+}
+
+function onRequestFinish (emitter, span) {
+  emitter.once('rows', () => {
+    span.finish()
+  })
+  emitter.once('error', (err) => {
+    span.setTag(Tags.ERROR, err)
+    span.finish()
+  })
+}
+
+function createWrapN1qlQuery (tracer, config) {
+  return function wrapQuery (_n1qlReq) {
+    return function queryWithTrace (host, q, adhoc, emitter) {
+      const scope = tracer.scope()
+      const query = q.statement
+      const bucket = this.name
+      const span = startQuerySpan('n1ql', query, tracer, config)
+
+      span.addTags({
+        'cluster.host': host,
+        bucket
+      })
+
+      onRequestFinish(emitter, span)
+
+      return scope.bind(_n1qlReq, span).apply(this, arguments)
+    }
+  }
+}
+
+function createWrapViewQuery (tracer, config) {
+  return function wrapQuery (_viewReq) {
+    return function queryWithTrace () {
+      const ddoc = arguments[1]
+      const viewName = arguments[2]
+
+      const scope = tracer.scope()
+      const span = startQuerySpan('view', viewName, tracer, config)
+
+      const bucket = this.name
+      span.addTags({
+        ddoc,
+        bucket
+      })
+
+      onRequestFinish(arguments[_viewReq.length - 1], span)
+
+      return scope.bind(_viewReq, span).apply(this, arguments)
+    }
+  }
+}
+
+function createWrapFtsQuery (tracer, config) {
+  return function wrapQuery (_ftsReq) {
+    return function queryWithTrace (q, emitter) {
+      const scope = tracer.scope()
+      const index = q.data.indexName
+      const span = startQuerySpan('search', index, tracer, config)
+
+      const bucket = this.name
+      span.setTag('bucket', bucket)
+
+      onRequestFinish(emitter, span)
+
+      return scope.bind(_ftsReq, span).apply(this, arguments)
+    }
+  }
+}
+
+function createWrapCbasQuery (tracer, config) {
+  return function wrapQuery (_cbasReq) {
+    return function queryWithTrace (host, q, emitter) {
+      const scope = tracer.scope()
+      const query = q.statement
+      const bucket = this.name
+      const span = startQuerySpan('cbas', query, tracer, config)
+
+      span.addTags({
+        'cbas.host': host,
+        bucket
+      })
+
+      onRequestFinish(emitter, span)
+
+      return scope.bind(_cbasReq, span).apply(this, arguments)
+    }
+  }
+}
+
+function createWrapOpenBucket (tracer) {
+  return function wrapOpenBucket (openBucket) {
+    return function openBucketWithTrace () {
+      const bucket = openBucket.apply(this, arguments)
+      return tracer.scope().bind(bucket)
+    }
+  }
+}
+
+module.exports = [
+  {
+    name: 'couchbase',
+    versions: ['>=2.2.0'],
+    file: 'lib/bucket.js',
+    patch (Bucket, tracer, config) {
+      this.wrap(Bucket.prototype, '_n1qlReq', createWrapN1qlQuery(tracer, config))
+      this.wrap(Bucket.prototype, '_viewReq', createWrapViewQuery(tracer, config))
+      this.wrap(Bucket.prototype, '_ftsReq', createWrapFtsQuery(tracer, config))
+      this.wrap(Bucket.prototype, '_cbasReq', createWrapCbasQuery(tracer, config))
+    },
+    unpatch (Bucket) {
+      this.unwrap(Bucket.prototype, '_n1qlReq')
+      this.unwrap(Bucket.prototype, '_viewReq')
+      this.unwrap(Bucket.prototype, '_ftsReq')
+      this.unwrap(Bucket.prototype, '_cbasReq')
+    }
+  },
+  {
+    name: 'couchbase',
+    versions: ['>=2.2.0'],
+    file: 'lib/cluster.js',
+    patch (Cluster, tracer, config) {
+      this.wrap(Cluster.prototype, 'openBucket', createWrapOpenBucket(tracer, config))
+    },
+    unpatch (Cluster) {
+      this.unwrap(Cluster.prototype, 'openBucket')
+    }
+  }
+]
---
+++ packages/datadog-plugin-couchbase/test/index.spec.js
@@ -0,0 +1,198 @@
+'use strict'
+
+const agent = require('../../dd-trace/test/plugins/agent')
+const plugin = require('../src')
+
+wrapIt()
+
+describe('Plugin', () => {
+  let couchbase
+  let N1qlQuery
+  let ViewQuery
+  let SearchQuery
+  let CbasQuery
+  let cluster
+  let bucket
+  let bucketManager
+  let platform
+  let tracer
+
+  describe('couchbase', () => {
+    withVersions(plugin, 'couchbase', version => {
+      beforeEach(() => {
+        platform = require('../../dd-trace/src/platform')
+        tracer = require('../../dd-trace')
+      })
+
+      describe('without configuration', () => {
+        beforeEach(() => {
+          return agent.load(plugin, 'couchbase').then(() => {
+            couchbase = require(`../../../versions/couchbase@${version}`).get()
+            N1qlQuery = couchbase.N1qlQuery
+            ViewQuery = couchbase.ViewQuery
+            SearchQuery = couchbase.SearchQuery
+            CbasQuery = couchbase.CbasQuery
+          })
+        })
+
+        beforeEach(done => {
+          cluster = new couchbase.Cluster('couchbase://localhost/')
+          cluster.authenticate('Administrator', 'password')
+          bucket = cluster.openBucket('datadog-test', (err) => {
+            if (err) done(err)
+
+            bucketManager = bucket.manager()
+            const map = `function (doc, meta) {
+                          if (doc.type && doc.type == 'landmark') {
+                            emit(doc.name, null)
+                          }
+                        }`
+            const ddocdata = { views: { by_name: { map } } }
+
+            bucketManager.upsertDesignDocument('datadoc', ddocdata, (err) => {
+              done(err)
+            })
+          })
+        })
+
+        afterEach(() => {
+          bucket.disconnect()
+        })
+
+        after(() => {
+          return agent.close()
+        })
+
+        describe('queries on cluster', () => {
+          it('should handle N1ql queries', done => {
+            const query = `UPSERT INTO \`datadog-test\` ( KEY, VALUE )
+                            VALUES (
+                              "landmark_1",
+                              {
+                                "id": "1",
+                                "type": "landmark",
+                                "name": "La Tour Eiffel",
+                                "location": "France"
+                              }
+                            )`
+
+            const n1qlQuery = N1qlQuery.fromString(query)
+
+            agent
+              .use(traces => {
+                const span = traces[0][0]
+                expect(span).to.have.property('name', 'couchbase.call')
+                expect(span).to.have.property('service', 'test-couchbase')
+                expect(span).to.have.property('resource', query)
+                expect(span).to.have.property('type', 'sql')
+                expect(span.meta).to.have.property('span.kind', 'client')
+                expect(span.meta).to.have.property('bucket', 'datadog-test')
+                expect(span.meta).to.have.property('query.type', 'n1ql')
+              })
+              .then(done)
+              .catch(done)
+
+            cluster.query(n1qlQuery, (err, rows, meta) => {
+              if (err) done(err)
+            })
+          })
+
+          it('should handle Search queries', done => {
+            const index = 'test'
+            const searchQuery = SearchQuery.new(index, SearchQuery.queryString('eiffel'))
+
+            agent
+              .use(traces => {
+                const span = traces[0][0]
+                expect(span).to.have.property('name', 'couchbase.call')
+                expect(span).to.have.property('service', 'test-couchbase')
+                expect(span).to.have.property('resource', index)
+                expect(span).to.have.property('type', 'sql')
+                expect(span.meta).to.have.property('span.kind', 'client')
+                expect(span.meta).to.have.property('bucket', 'datadog-test')
+                expect(span.meta).to.have.property('query.type', 'search')
+              })
+              .then(done)
+              .catch(done)
+
+            cluster.query(searchQuery, (err, rows, meta) => {
+              if (err) done(err)
+              expect(rows.length).to.not.equal(0)
+            })
+          })
+
+          it('should handle cbas queries', done => {
+            const query = 'SELECT * FROM airlines'
+            const cbasQuery = CbasQuery.fromString(query)
+
+            agent
+              .use(traces => {
+                const span = traces[0][0]
+                expect(span).to.have.property('name', 'couchbase.call')
+                expect(span).to.have.property('service', 'test-couchbase')
+                expect(span).to.have.property('resource', query)
+                expect(span).to.have.property('type', 'sql')
+                expect(span.meta).to.have.property('span.kind', 'client')
+                expect(span.meta).to.have.property('bucket', 'datadog-test')
+                expect(span.meta).to.have.property('query.type', 'cbas')
+              })
+              .then(done)
+              .catch(done)
+
+            cluster.query(cbasQuery, (err, rows, meta) => {
+              if (err) done(err)
+            })
+          })
+        })
+
+        describe('queries on buckets', () => {
+          it('should handle N1ql queries', done => {
+            const query = 'SELECT 1+1'
+            const n1qlQuery = N1qlQuery.fromString(query)
+
+            agent
+              .use(traces => {
+                const span = traces[0][0]
+                expect(span).to.have.property('name', 'couchbase.call')
+                expect(span).to.have.property('service', 'test-couchbase')
+                expect(span).to.have.property('resource', query)
+                expect(span).to.have.property('type', 'sql')
+                expect(span.meta).to.have.property('span.kind', 'client')
+                expect(span.meta).to.have.property('bucket', 'datadog-test')
+                expect(span.meta).to.have.property('query.type', 'n1ql')
+              })
+              .then(done)
+              .catch(done)
+
+            cluster.query(n1qlQuery, (err, rows, meta) => {
+              if (err) done(err)
+            })
+          })
+
+          it('should handle View queries ', done => {
+            const viewQuery = ViewQuery.from('datadoc', 'by_name')
+
+            agent
+              .use(traces => {
+                const span = traces[0][0]
+                expect(span).to.have.property('name', 'couchbase.call')
+                expect(span).to.have.property('service', 'test-couchbase')
+                expect(span).to.have.property('resource', viewQuery.name)
+                expect(span).to.have.property('type', 'sql')
+                expect(span.meta).to.have.property('span.kind', 'client')
+                expect(span.meta).to.have.property('bucket', 'datadog-test')
+                expect(span.meta).to.have.property('ddoc', viewQuery.ddoc)
+              })
+              .then(done)
+              .catch(done)
+
+            bucket.query(viewQuery, (err, rows, meta) => {
+              if (err) done(err)
+              expect(rows.length).to.not.equal(0)
+            })
+          })
+        })
+      })
+    })
+  })
+})
